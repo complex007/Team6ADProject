c0ab5e874dfb94c94334c7a2d8cd7eacf410c962
ACMH1 <icerimmedsteel@gmail.com>
2017-01-29 23:01:45 +0800

    Modified Login page.

--- Login.aspx.cs
+++ Login.aspx.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Security;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -10,5 +11,31 @@ public partial class Login : System.Web.UI.Page
     protected void Page_Load(object sender, EventArgs e)
     {
 
+    }
+
+    protected void Login1_LoggedIn(object sender, EventArgs e)
+    {
+        string userRole = Roles.GetRolesForUser(Login1.UserName)[0];
+        switch (userRole)
+        {
+            case "departmentemployee":
+            case "departmentrepresentative":
+                Response.Redirect("~/Department/DErequestItem.aspx");
+                break;
+            case "departmenthead":
+                Response.Redirect("~/Department/DHapproveReject.aspx");
+                break;
+            case "storeclerk":
+                Response.Redirect("~/Department/DHapproveReject.aspx");
+                break;
+            case "storesupervisor":
+            case "storemanager":
+                Response.Redirect("~/Store/SSapproveRejectOrder.aspx");
+                break;
+            default:
+                FormsAuthentication.SignOut();
+                FormsAuthentication.RedirectToLoginPage();
+                break;
+        }
     }
 }
